56e985588425e7ed24549845a702fd7af20e82a0
Cy-Bert <cyril.humbert.70@gmail.com>
2023-03-28 14:17:15 +0200

    Merge citation to affichage_citation

--- citations.js
+++ citations.js
@@ -1,21 +1,3 @@
-const base_citations =[
-    ["« Beaucoup pensent à vivre longtemps, peu à bien vivre. »", "Socrate"],
-    ["« Deux années de ma vie valent mieux qu'une lettre d'introduction. »", "Marcel Achard"],
-    ["« Ma vie entière s'est portée sur les films et la religion. C'est tout. Rien d'autre. »","Martin Scorsese"],
-    ["« La vie d’un homme n’est que d’un instant. »","Marc Aurèle"],
-    ["« La vie, c’est apprendre à aimer. »","Abbé Pierré"],
-    ["« Une belle vie, c’est une vie où on a beaucoup souffert. »","Christian Bobin"],
-    ["« La qualité de votre vie est égale à la qualité de vos pensées. »","Anthony Robbins"],
-    ["« Fais de ta vie un rêve, et d’un rêve, une réalité. »","Antoine de Saint Exupéry"],
-    ["« Il faut faire de sa vie un chef d’œuvre. »","Goethe"],
-    ["« Là où il y a l’amour, il y a la vie. »","Gandhi"],
-    
-];
-
-let index = Math.floor(Math.random() * (base_citations.length))
-
-base_citations[index][0];
-base_citations[index][1];
 const btn = document.querySelector("#button");
 // const text = document.querySelector("#answer");
 
@@ -35,6 +17,24 @@ function afficher() {
     // text.textContent = '';
 document.querySelector('#autor').innerHTML =  cit1['autor'];
 document.querySelector('#answer').innerHTML = cit1['citation'];
+const base_citations =[
+    ["« Beaucoup pensent à vivre longtemps, peu à bien vivre. »", "Socrate"],
+    ["« Deux années de ma vie valent mieux qu'une lettre d'introduction. »", "Marcel Achard"],
+    ["« Ma vie entière s'est portée sur les films et la religion. C'est tout. Rien d'autre. »","Martin Scorsese"],
+    ["« La vie d’un homme n’est que d’un instant. »","Marc Aurèle"],
+    ["« La vie, c’est apprendre à aimer. »","Abbé Pierré"],
+    ["« Une belle vie, c’est une vie où on a beaucoup souffert. »","Christian Bobin"],
+    ["« La qualité de votre vie est égale à la qualité de vos pensées. »","Anthony Robbins"],
+    ["« Fais de ta vie un rêve, et d’un rêve, une réalité. »","Antoine de Saint Exupéry"],
+    ["« Il faut faire de sa vie un chef d’œuvre. »","Goethe"],
+    ["« Là où il y a l’amour, il y a la vie. »","Gandhi"],
+    
+];
+
+let index = Math.floor(Math.random() * (base_citations.length))
+
+base_citations[index][0];
+base_citations[index][1];
 
 document.querySelector('#autor2').innerHTML =  cit2['autor'];
 document.querySelector('#answer2').innerHTML = cit2['citation'];
