6040b31c26488bb948fe0bbb542abe3e7fc23088
Cy-Bert <cyril.humbert.70@gmail.com>
2023-03-29 16:17:59 +0200

    add id btn done & edit

--- todolist.js
+++ todolist.js
@@ -10,23 +10,25 @@ function afficher() {
     const note = text.value;
     console.log(note);
     const div = document.createElement('div');
-    div.innerHTML=note;
-    div.classList.add("flex","justify-between","py-2");
+    div.innerHTML = note;
+    div.classList.add("flex", "justify-between", "py-2");
     todolist.appendChild(div);
-    text.value="";
+    text.value = "";
 
     const divbutton = document.createElement('div');
-    divbutton.classList.add("flex","flex-row","gap-x-1");
+    divbutton.classList.add("flex", "flex-row", "gap-x-1");
     div.appendChild(divbutton);
 
     const buttonD = document.createElement('button');
     buttonD.innerHTML = "Done";
-    buttonD.classList.add("w-10","p-1","sm:w-24","rounded", "border-black", "shadow", "bg-cyan-400", "text-white", "text-xs","hover:bg-cyan-500");
+    buttonD.classList.add("w-10", "p-1", "sm:w-24", "rounded", "border-black", "shadow", "bg-cyan-400", "text-white", "text-xs", "hover:bg-cyan-500");
+    buttonD.setAttribute("id", "btn_done")
     divbutton.appendChild(buttonD);
 
     const buttonE = document.createElement('button');
     buttonE.innerHTML = "Edit";
-    buttonE.classList.add("w-10","p-1","sm:w-24","rounded", "border-black", "shadow", "bg-cyan-400", "text-white", "text-xs","hover:bg-cyan-500");
+    buttonE.classList.add("w-10", "p-1", "sm:w-24", "rounded", "border-black", "shadow", "bg-cyan-400", "text-white", "text-xs", "hover:bg-cyan-500");
+    buttonE.setAttribute("id", "btn_edit")
     divbutton.appendChild(buttonE);
 }
 
