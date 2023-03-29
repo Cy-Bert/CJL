1cc663c9ac6606919ef953c243853abfae164731
grisburt <grisburt@gmail.com>
2023-03-29 11:45:01 +0200

    ajout note js

--- todolist.js
+++ todolist.js
@@ -1,4 +1,17 @@
-const xxx = document.querySelector ('#');
-cosnt yyy = document.querySelector ('#');
-const zzz = document.querySelector ('#');
+const text = document.querySelector('#textarea');
+const btn = document.querySelector('#button');
+const add = document.querySelector('#divareatodolist');
+
+btn.addEventListener('click', afficher);
+
+function afficher() {
+    //recuperation contenu et ajout div
+
+    const note = text.value;
+    console.log(note);
+    const div = document.createElement('div');
+    div.innerHTML=note;
+    add.appendChild(div);
+    text.value="";
+}
 
