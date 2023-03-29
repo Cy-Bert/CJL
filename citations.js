1cd65a0e18de83e19375036516fb186cbf463f27
Cy-Bert <125924936+Cy-Bert@users.noreply.github.com>
2023-03-29 22:46:04 +0200

    + navbar avec  burger  + mise en page + comment js

--- citations.js
+++ citations.js
@@ -1,15 +1,38 @@
+// NavBar
+        const menu_button = document.getElementById("menu-toggle");
+        const menu = document.getElementById("mobile-menu");
+
+        menu_button.addEventListener("click", () => {
+          // Toggle the "hidden" class on the menu
+          menu.classList.toggle("hidden");
+
+          // Toggle the "hidden" class on the button icon
+          menu_button.querySelectorAll("svg").forEach((icon) => {
+            icon.classList.toggle("hidden");
+          });
+
+          // Set the aria-expanded attribute
+          const expanded =
+           menu_button.getAttribute("aria-expanded") === "true" || false;
+         menu_button.setAttribute("aria-expanded", !expanded);
+        });
+
+        // Liste de citations
+
 const base_citations = [
     ["« Beaucoup pensent à vivre longtemps, peu à bien vivre. »", "Socrate"],
     ["« Deux années de ma vie valent mieux qu'une lettre d'introduction. »", "Marcel Achard"],
     ["« Ma vie entière s'est portée sur les films et la religion. C'est tout. Rien d'autre. »", "Martin Scorsese"],
     ["« La vie d’un homme n’est que d’un instant. »", "Marc Aurèle"],
-    ["« La vie, c’est apprendre à aimer. »", "Abbé Pierré"],
+    ["« La vie, c’est apprendre à aimer. »", "Abbé Pierre"],
     ["« Une belle vie, c’est une vie où on a beaucoup souffert. »", "Christian Bobin"],
     ["« La qualité de votre vie est égale à la qualité de vos pensées. »", "Anthony Robbins"],
     ["« Fais de ta vie un rêve, et d’un rêve, une réalité. »", "Antoine de Saint Exupéry"],
     ["« Il faut faire de sa vie un chef d’œuvre. »", "Goethe"],
     ["« Là où il y a l’amour, il y a la vie. »", "Gandhi"],
 ];
+
+//Génarateur
 
 const btn = document.querySelector("#button");
 const autor = document.querySelector('#autor');
