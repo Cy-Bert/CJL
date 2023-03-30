85cb26a3e415a08f2957fd03b9bec42128f29d82
Cy-Bert <cyril.humbert.70@gmail.com>
2023-03-30 11:35:25 +0200

    Delete task

--- todolist.js
+++ todolist.js
@@ -25,22 +25,15 @@ function afficher() {
     buttonD.classList.add("w-10", "p-1", "sm:w-24", "rounded", "border-black", "shadow", "bg-cyan-400", "text-white", "text-xs", "hover:bg-cyan-500");
     buttonD.setAttribute("id", "btn_done")
     divbutton.appendChild(buttonD);
+    buttonD.addEventListener('click', deleting);
 
-    const buttonE = document.createElement('button');
-    buttonE.innerHTML = "Edit";
-    buttonE.classList.add("w-10", "p-1", "sm:w-24", "rounded", "border-black", "shadow", "bg-cyan-400", "text-white", "text-xs", "hover:bg-cyan-500");
-    buttonE.setAttribute("id", "btn_edit")
-    divbutton.appendChild(buttonE);
+    // const buttonE = document.createElement('button');
+    // buttonE.innerHTML = "Edit";
+    // buttonE.classList.add("w-10", "p-1", "sm:w-24", "rounded", "border-black", "shadow", "bg-cyan-400", "text-white", "text-xs", "hover:bg-cyan-500");
+    // buttonE.setAttribute("id", "btn_edit")
+    // divbutton.appendChild(buttonE);
 }
-
-//bouton "done"
-const doneBtn = document.querySelector('.btn_done');
-//click sur le boutton done
-doneBtn.addEventListener('click', () => {
-    const task = doneBtn.parentNode;
-    //liste taches effectuées
-    const taskdown = document.querySelector('#taskdown ul');
-
-    // liste à faire devient done
-    taskdown.appendChild(task);
-});
+function deleting() {
+    let PapyDiv = this.parentElement.parentElement;
+    PapyDiv.remove()
+}
