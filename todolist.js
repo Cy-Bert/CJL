ff4ee6754a9a1b3df81f0f37fc4eb1d4f83f980d
Cy-Bert <cyril.humbert.70@gmail.com>
2023-03-30 08:44:56 +0200

    merge

--- todolist.js
+++ todolist.js
@@ -36,4 +36,14 @@ function afficher() {
     divbutton.appendChild(buttonE);
 }
 
+//bouton "done"
+const doneBtn = document.querySelector('.btn_done');
+//click sur le boutton done
+doneBtn.addEventListener('click', () => {
+    const task = doneBtn.parentNode;
+    //liste taches effectuées
+    const taskdown = document.querySelector('#taskdown ul');
 
+    // liste à faire devient done
+    taskdown.appendChild(task);
+});
