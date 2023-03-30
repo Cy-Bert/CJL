1b8fea93a9aa678b5e9f1ed7f7f514b87fadc18f
Julo25 <modjane@gmail.com>
2023-03-30 16:04:31 +0200

    update_compteur
    les 3 codes sonts liés
    css retravaillé

--- count.js
+++ count.js
@@ -1,12 +1,13 @@
-var nombreClics = 0;
 
-function comptage() {
-    nombreClics++;
+
+    let nombreClics = 0;
+
+    function comptage() {
+        nombreClics++;
     document.getElementById("nombreClics").textContent = nombreClics;
 }
 
-document.getElementById("boutonClic").addEventListener("click", comptage);
+    document.getElementById("boutonClic").addEventListener("click", comptage);
 
-document.getElementById("desactiver").addEventListener("click", function (e) {
-    document.getElementById("boutonClic").removeEventListener("click", comptage);
-});
+    document.getElementById("desactiver").addEventListener("click");
+// document.getElementById("boutonClic").removeEventListener("click", comptage);
