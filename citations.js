fd4ac551ca1d80c5f5cea831b18ab0afc2582771
Cy-Bert <cyril.humbert.70@gmail.com>
2023-03-30 11:54:40 +0200

    Release V 0.45

--- citations.js
+++ citations.js
@@ -1,22 +1,3 @@
-// NavBar
-        const menu_button = document.getElementById("menu-toggle");
-        const menu = document.getElementById("mobile-menu");
-
-        menu_button.addEventListener("click", () => {
-          // Toggle the "hidden" class on the menu
-          menu.classList.toggle("hidden");
-
-          // Toggle the "hidden" class on the button icon
-          menu_button.querySelectorAll("svg").forEach((icon) => {
-            icon.classList.toggle("hidden");
-          });
-
-          // Set the aria-expanded attribute
-          const expanded =
-           menu_button.getAttribute("aria-expanded") === "true" || false;
-         menu_button.setAttribute("aria-expanded", !expanded);
-        });
-
         // Liste de citations
 
 const base_citations = [
